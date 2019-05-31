b1ffdf6c9a059ab4b99a8dccd76077ad7c75e55e
alexbeattie42 <alexbeattie42@gmail.com>
2019-05-31 12:06:42 +0200

    Lnu Thesis Manager Login Registration
     - Update Login Registration

--- src/main/java/se/lnu/controllers/MainController.java
+++ src/main/java/se/lnu/controllers/MainController.java
@@ -7,18 +7,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import se.lnu.dao.DocumentDao;
 import se.lnu.dao.UserDao;
 import se.lnu.entity.Document;
 import se.lnu.entity.Role;
 import se.lnu.entity.User;
 
-@RequestMapping("")
+@RequestMapping("/test")
 @Controller
 public class MainController {
     final static Logger LOG = LoggerFactory.getLogger(MainController.class);
 
    // @Autowired
    //private UserDao userDao;
+    @Autowired
+    private DocumentDao documentDao;
 
     @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model) {
@@ -26,9 +29,10 @@ public class MainController {
 //        role.setDescription("admin");
 //        role = userDao.saveRole(role);
 //
-//        Document document = new Document();
-//        document.setFilePath("/u01/test");
-//        document.setTitle("TestDoc");
+        Document document = new Document();
+        document.setFilePath("/u01/test");
+        document.setTitle("TestDoc");
+        documentDao.saveDocument(document);
 //
 //        User user = new User();
 //        user.setFirstName("John");
